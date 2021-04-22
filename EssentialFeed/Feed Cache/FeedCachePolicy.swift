bc4db1795311fbcc41b36a41957f90a977c41dd7
oligazar <nikelodian@gmail.com>
2021-04-22 21:59:58 +0300

    Move 'FeedCachePolicy' to its own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Alexander on 22/4/21.
+//
+
+import Foundation
+
+internal final class FeedCachePolicy {
+    private init() {}
+    
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    internal static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
