c1f61020265d8bbe8a245837276301e028225b5a
oligazar <nikelodian@gmail.com>
2021-05-03 20:54:54 +0300

    "CoreDataFeedStore.retrieve()' delivers found values on non empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -7,6 +7,19 @@
 
 import CoreData
 
+fileprivate extension Array where Element == LocalFeedImage {
+    func toMamangedFeed(context: NSManagedObjectContext) -> [ManagedFeedImage] {
+        return map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        }
+    }
+}
+
 public class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
@@ -21,11 +34,40 @@ public class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
+        let context = self.context
+        context.perform {
+            do {
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = NSOrderedSet(array: feed.toMamangedFeed(context: context))
+                
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        let context = self.context
+        context.perform {
+            do {
+                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+                request.returnsObjectsAsFaults = false
+                if let cache = try context.fetch(request).first {
+                    let feed = cache.feed.compactMap { ($0 as? ManagedFeedImage ) }
+                        .map {
+                            LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+                        }
+                    completion(.found(feed: feed, timestamp: cache.timestamp))
+                } else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
 }
 
@@ -62,12 +104,14 @@ private extension NSManagedObjectModel {
     }
 }
 
+@objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
 }
 
-private class ManagedFeedItem: NSManagedObject {
+@objc(ManagedFeedImage)
+private class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
