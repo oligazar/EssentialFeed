e7fb01bcd65aaad2786e4cb1d9160ec5364bc2f3
oligazar <nikelodian@gmail.com>
2021-05-23 08:12:00 +0300

    Render loaded feed on successful load completion

---
+++ EssentialFeed/Feed Cache/EssentialPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "Hello, playground"
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    let origin: Int
+    let size: Int
+}
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: 1, size: 2)
+    }
+}
+
+
+
---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Alexander on 23/5/21.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
