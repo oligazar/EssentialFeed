6a0a46db8476b7c336da3a8e4ae4acf4746aabf0
oligazar <nikelodian@gmail.com>
2021-05-24 06:38:01 +0300

    Preload image url when image view is near visible

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -11,12 +11,13 @@ import EssentialFeed
 public protocol FeedImageDataLoaderTask {
     func cancel()
 }
+
 public protocol FeedImageDataLoader {
     typealias Result = Swift.Result<Data, Error>
     func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }
 
-final public class FeedViewController: UITableViewController {
+final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
@@ -33,6 +34,7 @@ final public class FeedViewController: UITableViewController {
         
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        tableView.prefetchDataSource = self
         
         load()
     }
@@ -70,7 +72,6 @@ final public class FeedViewController: UITableViewController {
                 } else {
                     cell?.feedImageRetryButton.isHidden = false
                 }
-                
                 cell?.feedImageContainer.stopShimmering()
             }
         }
@@ -84,5 +85,12 @@ final public class FeedViewController: UITableViewController {
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         imageDataLoadingTasks[indexPath]?.cancel()
         imageDataLoadingTasks[indexPath] = nil
+    }
+    
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach { indexPath in
+            let cellModel = tableModel[indexPath.row]
+            _ = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+        }
     }
 }
