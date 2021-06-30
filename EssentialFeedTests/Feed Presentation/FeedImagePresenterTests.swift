12df1f24482c63b7034af82ca471f704c3076dc0
oligazar <nikelodian@gmail.com>
2021-06-30 09:30:21 +0300

    Assure that didStartLoadingImage displays loading indicator, no image and shouldRetry == false

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 import XCTest
+import EssentialFeed
 
-struct FeedImageViewModel<Image> {
+struct FeedImageViewModel<Image: Equatable>: Equatable {
     
     let location: String?
     let description: String?
@@ -22,15 +23,25 @@ struct FeedImageViewModel<Image> {
 }
 
 protocol FeedImageView {
-    associatedtype Image
+    associatedtype Image: Equatable
     
     func display(_ model: FeedImageViewModel<Image>)
 }
 
 class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
     
     init(view: View) {
-        
+        self.view = view
+    }
+    
+    func didStartLoadingImage(for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            location: model.location,
+            description: model.description,
+            image: nil, isImageLoading: true,
+            shouldRetry: false
+        ))
     }
 }
 
@@ -40,10 +51,33 @@ class FeedImagePresenterTests: XCTestCase {
         let view = ViewSpy()
         let _ = FeedImagePresenter(view: view)
         
-        XCTAssertTrue(view.messages.isEmpty)
+        XCTAssertEqual(view.messages, [])
+    }
+    
+    func test_didStartLoadingImage_displaysLoadingIndicatorAndNoImage() {
+        let (sut, view) = makeSut()
+        let model = FeedImage(id: UUID(), description: "start loading", location: "any location", url: URL(string: "http://image.url")!)
+        
+        let result = FeedImageViewModel<String>(
+            location: model.location,
+            description: model.description,
+            image: nil,
+            isImageLoading: true, shouldRetry: false)
+
+        sut.didStartLoadingImage(for: model)
+
+        XCTAssertEqual(view.messages[0], result)
     }
     
     // MARK: helpers
+    
+    private func makeSut(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImagePresenter<ViewSpy, String>, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackMemoryLeaks(view)
+        trackMemoryLeaks(sut)
+        return (sut, view)
+    }
     
     private class ViewSpy: FeedImageView {
         typealias Image = String
