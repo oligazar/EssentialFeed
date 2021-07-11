d03c89d232f13ec43a724cc5e9289bd48484f62f
oligazar <nikelodian@gmail.com>
2021-07-11 11:52:47 +0300

    Move `CoreDataFeedStore` extension implementing `FeedImageDataStore` protocol to production

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -9,17 +9,6 @@ import Foundation
 import EssentialFeed
 import XCTest
 
-extension CoreDataFeedStore: FeedImageDataStore {
-    
-    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        
-    }
-    
-    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(.none))
-    }
-}
-
 class CoreDataFeedImageDataStoreTests: XCTestCase {
     
     func test_retrieveImageData_deliversImageDataNotFoundWhenEmpty() {
