4b0f37487a221849f3a16b783cecab9f28de48f9
oligazar <nikelodian@gmail.com>
2021-07-21 11:13:19 +0300

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` delivers error on both primary and fallback loader failure

--- EssentialAppTests/RemoteWithLocalFallbackFeedImageDataLoaderTests.swift
+++ EssentialAppTests/RemoteWithLocalFallbackFeedImageDataLoaderTests.swift
@@ -110,6 +110,15 @@ class RemoteWithLocalFallbackFeedImageDataLoaderTests: XCTestCase {
         })
     }
     
+    func test_loadImageData_deliversErrorOnBothPrimaryAndFallbackLoadersFailure() {
+        let (sut, primaryLoader, fallbackLoader) = makeSUT()
+        
+        expect(sut, toCompleteWith: .failure(anyNSError()), when: {
+            primaryLoader.complete(with: anyNSError())
+            fallbackLoader.complete(with: anyNSError())
+        })
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImageDataLoader, primary: LoaderSpy, fallback: LoaderSpy) {
