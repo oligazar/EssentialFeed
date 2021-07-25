9ad99357b08339bf0c4e4e63c5e2e0e889bd89b6
oligazar <nikelodian@gmail.com>
2021-07-25 10:03:09 +0300

    Inline `Data` creation

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -91,9 +91,8 @@ class FeedImagePresenterTests: XCTestCase {
     func test_didStartLoadingImageData_displaysRetryOnFailedImageTransformation() {
         let (sut, view) = makeSut(imageTransformer: fail)
         let image = uniqueImage()
-        let data = Data()
         
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
@@ -107,11 +106,10 @@ class FeedImagePresenterTests: XCTestCase {
     
     func test_didStartLoadingImageData_displaysImageOnSuccessfullTransformation() {
         let image = uniqueImage()
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSut(imageTransformer: { _ in transformedData })
         
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
         
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
